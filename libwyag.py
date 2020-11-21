1fef0b5a1e232a937643e4a3c44130c00c7a62ff
sueka <mail@sueka.me>
2020-11-22 01:11:32 +0900

    :sparkles: Create argsubparsers

--- libwyag.py
+++ libwyag.py
@@ -23,3 +23,6 @@
 import argparse, collections, configparser, hashlib, os, re, sys, zlib
 
 argparser = argparse.ArgumentParser(description="The stupid content tracker")
+
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
