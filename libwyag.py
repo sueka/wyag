4a98d7daf2be31afa2f0a87fff0f433eafd84983
sueka <mail@sueka.me>
2020-11-25 02:43:09 +0900

    :sparkles: Add GitBlob class

--- libwyag.py
+++ libwyag.py
@@ -247,3 +247,12 @@ def object_write(obj, actually_write=True):
             f.write(zlib.compress(result))
 
     return sha
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
