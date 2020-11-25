d0b1d994ae1526a4518d090f3993a0d68d9d3b0d
sueka <mail@sueka.me>
2020-11-25 19:56:26 +0900

    :sparkles: Let you dump a Graphviz data to render the log

--- libwyag.py
+++ libwyag.py
@@ -403,3 +403,33 @@ argsp.add_argument("commit",
                    default="HEAD",
                    nargs="?",
                    help="Commit to start at.")
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+def log_graphviz(repo, sha, seen):
+
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    assert (commit.fmt==b'commit')
+
+    if not b'parent' in commit.kvlm.keys():
+        # Base case: the initial commit.
+        return
+
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [ parents ]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print ("c_{0} -> c_{1};".format(sha, p))
+        log_graphviz(repo, p, seen)
