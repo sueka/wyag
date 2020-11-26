6f787b85dac45aac2e0eb2f780b83124cd142c48
sueka <mail@sueka.me>
2020-11-27 05:05:28 +0900

    :sparkles: Enable you to make refs flattened a dict

--- libwyag.py
+++ libwyag.py
@@ -557,3 +557,18 @@ def ref_resolve(repo, ref):
         return ref_resolve(repo, data[5:])
     else:
         return data
+
+def ref_list(repo, path=None):
+    if not path:
+        path = repo_dir(repo, "refs")
+    ret = collections.OrderedDict()
+    # Git shows refs sorted.  To do the same, we use
+    # an OrderedDict and sort the output of listdir
+    for f in sorted(os.listdir(path)):
+        can = os.path.join(path, f)
+        if os.path.isdir(can):
+            ret[f] = ref_list(repo, can)
+        else:
+            ret[f] = ref_resolve(repo, can)
+
+    return ret
