a911a0c5bb18373f3efeb6687f3e30ae17f78080
sueka <mail@sueka.me>
2020-11-27 05:06:14 +0900

    :sparkles: Make the wyag show-ref command

--- libwyag.py
+++ libwyag.py
@@ -572,3 +572,20 @@ def ref_list(repo, path=None):
             ret[f] = ref_resolve(repo, can)
 
     return ret
+
+argsp = argsubparsers.add_parser("show-ref", help="List references.")
+
+def cmd_show_ref(args):
+    repo = repo_find()
+    refs = ref_list(repo)
+    show_ref(repo, refs, prefix="refs")
+
+def show_ref(repo, refs, with_hash=True, prefix=""):
+    for k, v in refs.items():
+        if type(v) == str:
+            print ("{0}{1}{2}".format(
+                v + " " if with_hash else "",
+                prefix + "/" if prefix else "",
+                k))
+        else:
+            show_ref(repo, v, with_hash=with_hash, prefix="{0}{1}{2}".format(prefix, "/" if prefix else "", k))
