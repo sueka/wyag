1deb75c2839f5d185315f0e220830b07f8325596
sueka <mail@sueka.me>
2020-11-26 13:37:11 +0900

    :sparkles: Enable you to parse a record of a tree

--- libwyag.py
+++ libwyag.py
@@ -439,3 +439,23 @@ class GitTreeLeaf(object):
         self.mode = mode
         self.path = path
         self.sha = sha
+
+def tree_parse_one(raw, start=0):
+    # Find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert(x-start == 5 or x-start==6)
+
+    # Read the mode
+    mode = raw[start:x]
+
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00', x)
+    # and read the path
+    path = raw[x+1:y]
+
+    # Read the SHA and convert to an hex string
+    sha = hex(
+        int.from_bytes(
+            raw[y+1:y+21], "big"))[2:] # hex() adds 0x in front,
+                                           # we don't want that.
+    return y+21, GitTreeLeaf(mode, path, sha)
