bd076ce6945f47b1e4a81a963d5496497936e5eb
sueka <mail@sueka.me>
2020-11-27 06:08:22 +0900

    :sparkles: Make the wyag tag available

--- libwyag.py
+++ libwyag.py
@@ -610,3 +610,14 @@ argsp.add_argument("object",
                     default="HEAD",
                     nargs="?",
                     help="The object the new tag will point to")
+
+def cmd_tag(args):
+    repo = repo_find()
+
+    if args.name:
+        tag_create(args.name,
+                   args.object,
+                   type="object" if args.create_tag_object else "ref")
+    else:
+        refs = ref_list(repo)
+        show_ref(repo, refs["tags"], with_hash=False)
