3139dd00f4d30f761fb1a7905927ba1aad436ebf
sueka <mail@sueka.me>
2020-11-27 09:57:27 +0900

    :sparkles: Enable you to resolve an object name

--- libwyag.py
+++ libwyag.py
@@ -621,3 +621,45 @@ def cmd_tag(args):
     else:
         refs = ref_list(repo)
         show_ref(repo, refs["tags"], with_hash=False)
+
+def object_resolve(repo, name):
+    """Resolve name to an object hash in repo.
+
+This function is aware of:
+
+ - the HEAD literal
+ - short and long hashes
+ - tags
+ - branches
+ - remote branches"""
+    candidates = list()
+    hashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+    smallHashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+
+    # Empty string?  Abort.
+    if not name.strip():
+        return None
+
+    # Head is nonambiguous
+    if name == "HEAD":
+        return [ ref_resolve(repo, "HEAD") ]
+
+
+    if hashRE.match(name):
+        if len(name) == 40:
+            # This is a complete hash
+            return [ name.lower() ]
+        elif len(name) >= 4:
+            # This is a small hash 4 seems to be the minimal length
+            # for git to consider something a short hash.
+            # This limit is documented in man git-rev-parse
+            name = name.lower()
+            prefix = name[0:2]
+            path = repo_dir(repo, "objects", prefix, mkdir=False)
+            if path:
+                rem = name[2:]
+                for f in os.listdir(path):
+                    if f.startswith(rem):
+                        candidates.append(prefix + f)
+
+    return candidates
